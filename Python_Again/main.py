0b600a0771de8423cd89d9f5245e81e76ce48f62
Anashali Saiyed <saiyedanashali@gmail.com>
2026-05-14 12:05:26 +0530

    upd

---
+++ Python_Again/main.py
@@ -0,0 +1,11 @@
+import  pandas as pd
+
+df = pd.read_csv('Book1.csv')
+#
+# print(df.head(2))
+# print(df.tail(1))
+# print(df.shape)
+# print(df.columns)
+# print(df.dtypes)
+# print(df.describe())
+# print(df.info())
